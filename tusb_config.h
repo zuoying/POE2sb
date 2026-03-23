a837e79747f06a1906c4728497373a5c35ca4e98
zuoying <36652747+zuoying@users.noreply.github.com>
2026-03-23 15:04:50 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -4,26 +4,21 @@
 #include <stdint.h>
 
 #define CFG_TUSB_OS               OPT_OS_PICO
-
 #define CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_ALIGN        __attribute__((aligned(4)))
-
 #define CFG_TUD_ENDPOINT0_SIZE    64
 
 #define CFG_TUD_ENABLED           1
-#define CFG_TUD_HID               1
-#define CFG_TUD_HID_EP_BUFSIZE    32
+#define CFG_TUD_HID              1
+#define CFG_TUD_HID_EP_BUFSIZE    64
 
 #define CFG_TUH_ENABLED           1
 #define CFG_TUH_RPI_PIO_USB       1
-#define CFG_TUH_HID               1
-#define CFG_TUH_DEVICE_MAX        2
+#define CFG_TUH_HID              1
+#define CFG_TUH_DEVICE_MAX        1
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 #define CFG_TUH_HID_EPIN_BUFSIZE  64
 #define CFG_TUH_HID_EPOUT_BUFSIZE 64
-
-#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
-#define CFG_TUSB_RHPORT1_MODE     (OPT_MODE_HOST | OPT_MODE_FULL_SPEED)
 
 #ifndef PICO_PIO_USB_PIN_DP
 #define PICO_PIO_USB_PIN_DP 12
