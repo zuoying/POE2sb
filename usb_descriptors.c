e532982b70482e1c6cf1e6704cc43bac56f1f6a2
zuoying <36652747+zuoying@users.noreply.github.com>
2026-03-23 16:06:47 +0800

    Update usb_descriptors.c

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -50,12 +50,8 @@ uint8_t const desc_configuration[] = {
     0x10,
 };
 
-uint8_t const desc_hid_report[] = {
-    TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE)
-};
-
-tusb_desc_device_t const* tud_descriptor_device_cb(void) {
-    return &desc_device;
+uint8_t const* tud_descriptor_device_cb(void) {
+    return (uint8_t const*) &desc_device;
 }
 
 uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
@@ -64,20 +60,6 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
 }
 
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
-    (void)langid;
+    (void)index;(void)langid;
     return NULL;
-}
-
-uint8_t const* tud_hid_descriptor_report_cb(uint8_t itf) {
-    (void)itf;
-    return desc_hid_report;
-}
-
-uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
-    (void)itf;(void)report_id;(void)report_type;(void)buffer;(void)reqlen;
-    return 0;
-}
-
-void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
-    (void)itf;(void)report_id;(void)report_type;(void)buffer;(void)bufsize;
 }
