11ad6193f351083b50676d15f87374266373132e
zuoying <36652747+zuoying@users.noreply.github.com>
2026-03-23 11:31:45 +0800

    Update main.c

--- main.c
+++ main.c
@@ -272,10 +272,11 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t typ
     (void)bufsize;
 }
 
-void tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t type, uint8_t* buffer, uint16_t reqlen) {
+uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t type, uint8_t* buffer, uint16_t reqlen) {
     (void)itf;
     (void)report_id;
     (void)type;
     (void)buffer;
     memset(buffer, 0, reqlen);
+    return reqlen;
 }
