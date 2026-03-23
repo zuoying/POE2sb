0a4d232abdad36db64f9c02497b56c47ef64a4d5
zuoying <36652747+zuoying@users.noreply.github.com>
2026-03-23 17:03:18 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -9,9 +9,8 @@
 #define CFG_TUD_ENDPOINT0_SIZE    64
 
 #define CFG_TUD_ENABLED           1
-#define CFG_TUD_CDC              1
-#define CFG_TUD_CDC_RX_BUFSIZE    256
-#define CFG_TUD_CDC_TX_BUFSIZE    256
+#define CFG_TUD_HID              1
+#define CFG_TUD_HID_EP_BUFSIZE    64
 
 #define CFG_TUH_ENABLED           0
 
